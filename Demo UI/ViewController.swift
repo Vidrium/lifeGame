e510aae3a88a933c3f7e72a07c02d392a4f50b40
Developer <developer@Mac-de-Developer.local>
2019-01-31 12:44:15 +0100

    Initial Commit

---
+++ Demo UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo UI
+//
+//  Created by Developer on 31/01/2019.
+//  Copyright © 2019 Ynov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
