b58584ac872835a661489d50b156d32ebf44dec9
Developer <developer@Mac-de-Developer.local>
2019-03-01 12:34:03 +0100

    Tiens michou

--- Demo UI/GameViewController.swift
+++ Demo UI/GameViewController.swift
@@ -8,23 +8,32 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
 
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var histoire: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let list = [1,2]
+        let list2 = ["1","2"]
+        
+        let situation = Situation(text: "A que coucou bonjour monsieur patate. comment allez-vus aujourd'hui? Trés bien et vous lol. je m'appelle Thomas le chien allemente t je mange des os de boeuf", destination: list, destinationText: list2)
+        
+        histoire.text = situation.text
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ Demo UI/Situation.swift
@@ -0,0 +1,20 @@
+//
+//  Situation.swift
+//  Demo UI
+//
+//  Created by Developer on 28/02/2019.
+//  Copyright © 2019 Ynov. All rights reserved.
+//
+
+import Foundation
+
+struct Situation {
+    
+    var text: String;
+    var destination : Array<Int>;
+    var destinationText : Array<String>;
+    var backgroud: Int;
+    var image: Int;
+    
+}
+
